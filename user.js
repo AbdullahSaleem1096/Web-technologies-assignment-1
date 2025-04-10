131869bba470602fa538880624f39b4c0695221e
FakihaSaadat26 <fakiha.saadat@gmail.com>
2025-04-10 23:50:49 +0500

    Maryum has done this work

---
+++ user.js
@@ -0,0 +1,22 @@
+// // users.js - Contains existing user data
+
+// // Array of existing users
+// const users = [
+//     {
+//         username: "maryum",
+//         email: "maryam@gmail.com",
+//         password: "helloworld" 
+//     },
+//     {
+//         username: "fakiha",
+//         email: "fakiha@gmail.com",
+//         password: "helloworld2"
+//     },
+//     {
+//         username: "admin",
+//         email: "admin@gmail.com",
+//         password: "admin123"
+//     }
+// ];
+
+// export { users };
